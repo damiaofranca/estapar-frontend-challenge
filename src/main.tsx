147c4caacc4ee7254af44e12342297531e6b5fb7
Damião França <francadamiao202@gmail.com>
2026-05-07 22:15:37 -0300

    refactor!: replace App shell with router bootstrap in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import "./index.css";
+import "react-toastify/ReactToastify.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createQueryClient } from "./lib/queryClient.tsx";
+import { ToastContainer } from "react-toastify";
+import { RouterProvider } from "react-router-dom";
 import { QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./App.tsx";
+import { createQueryClient } from "./lib/queryClient.tsx";
 
+import { router } from "./router/index.tsx";
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={createQueryClient}>
-			<App />
+			<RouterProvider router={router} />
+			<ToastContainer />
 		</QueryClientProvider>
 	</StrictMode>,
 );
